95428eae1ec54ba0590fe1b7dbc5f8b509f7b4af
Andrzej Gębala <agebala@neonet.pl>
2023-07-20 16:28:38 +0200

    adding js to qiuz

--- assets/db.js
+++ assets/db.js
@@ -18,6 +18,42 @@ const quiz = [
         answers: ['Paryż', 'Madryd', 'Warszawa'],
         correctAnswer: 'Madryd'
     },
+    {
+        id: 4,
+        question: "co jest stolicą Grecji ?",
+        answers: ['Ateny', 'Praga', 'Koziołkowo'],
+        correctAnswer: 'Ateny'
+    },
+    {
+        id: 5,
+        question: "jaki jest wynik, 25 + 13 ?",
+        answers: ['36', '38', '40'],
+        correctAnswer: '38'
+    },
+    {
+        id: 6,
+        question: "jaki jest wynik, 43 - 13 ?",
+        answers: ['22', '33', '30'],
+        correctAnswer: '30'
+    },
+    {
+        id: 7,
+        question: "jakie oznaczenie po angielsku to popołudnie ?",
+        answers: ['PM', 'AM', 'Żadna'],
+        correctAnswer: 'PM'
+    },
+    {
+        id: 8,
+        question: "ile lat mają wspólnie Twoi rodzice ?",
+        answers: ['70', '76', '78'],
+        correctAnswer: '78'
+    },
+    {
+        id: 9,
+        question: "kto potrafi latać ?",
+        answers: ['Batman', 'Spiderman', 'Supermen'],
+        correctAnswer: 'Supermen'
+    },
 ]
 
 
--- assets/script.js
+++ assets/script.js
@@ -2,22 +2,29 @@ let questionNumber = document.querySelector(".question__title--number")
 let questionContent = document.querySelector(".question__content")
 let answerContent = document.querySelectorAll(".answer-version__content")
 const nextBtn = document.querySelector(".next-btn")
+const resltBtn = document.querySelector(".result-btn")
 const startBtn = document.querySelector(".start-btn")
 const mainView = document.querySelector(".main")
 let sec = document.querySelector(".counter__sec")
 let min = document.querySelector(".counter__min")
+let questionsValue = document.querySelector(".quiz-overall__value")
+let score = document.querySelector(".user-score__value")
+let allQuestions = document.querySelector(".all-questions")
+const modal = document.querySelector(".modal")
+const closeModals = document.querySelector(".modal button")
 
 let userAnswersTable = []
 let userChoose;
-let userAnswersJSON = localStorage.getItem('userAnswersTable')
-let userAnswers = JSON.parse(userAnswersJSON)
-console.log(userAnswers)
+
+
 
 import quiz from "./db.js";
 let secCounter = 0
 let minutes = 0
 let questionsCounter = 1
 let isAnswer = false;
+let currentAnswers = quiz.map(el => el.correctAnswer)
+
 
 
 let timeDisplay = () => {
@@ -46,6 +53,8 @@ let timeDisplay = () => {
 }
 
 const startQiuz = () => {
+    nextBtn.classList.remove("hide")
+    nextBtn.disabled = false
     localStorage.removeItem('userAnswersTable')
     startBtn.classList.add("hide")
     mainView.classList.remove("hide")
@@ -71,7 +80,7 @@ answerContent.forEach((answer, indexAnswers) => {
 });
 
 const nextQuestion = () => {
-    
+
     answerContent.forEach(el => {
         el.classList.remove("active")
     })
@@ -79,13 +88,18 @@ const nextQuestion = () => {
         nextBtn.textContent = "Wybierz odpowiedź"
     }
     else {
+        if (questionsCounter >= quiz.length) {
+            nextBtn.disabled = true
+            nextBtn.classList.add("hide")
+            resltBtn.classList.remove("hide")
+            mainView.classList.add("hide")
+        }
         userAnswersTable.push(userChoose)
-        userAnswersJSON = JSON.stringify(userAnswersTable)
+        let userAnswersJSON = JSON.stringify(userAnswersTable)
         localStorage.setItem('userAnswersTable', userAnswersJSON)
 
         nextBtn.textContent = "następne pytanie"
-
-        questionNumber.textContent = questionsCounter + 1
+        questionNumber.textContent = questionsCounter
         questionContent.textContent = quiz[questionsCounter].question
         answerContent.forEach((answer, indexAnswers) => {
             quiz[questionsCounter].answers.forEach((el, i) => {
@@ -97,14 +111,68 @@ const nextQuestion = () => {
         questionsCounter++
         isAnswer = false
     }
-    if (questionsCounter === quiz.length) {
-       return console.log("end")
-    }
 }
 
+const quizResult = () => {
+    questionsCounter = 1;
+    mainView.classList.add("hide")
+    modal.classList.remove("hide")
+    let userAnswersJSON = localStorage.getItem('userAnswersTable')
+    let userAnswers = JSON.parse(userAnswersJSON)
+
+    questionsValue.textContent = quiz.length
+    score.textContent = currentAnswers.filter((el ,i) =>el === userAnswers[i]).length
+    for (let i = 0; i < quiz.length; i++) {
+
+        let questionCount = document.createElement('p')
+        let answerCont = document.createElement("ul")
+        questionCount.classList.add("modal-question")
+        questionCount.textContent = quiz[i].question
+
+        for (let j = 0; j < quiz[i].answers.length; j++) {
+
+            const answerList = document.createElement("li");
+            answerList.classList.add("modal-answer")
+            answerList.textContent = quiz[i].answers[j];
+            if (userAnswers[i] !== currentAnswers[i]) {
+                if (quiz[i].answers[j] === currentAnswers[i]) {
+                    answerList.style.color = "green";
+
+                }
+                if (quiz[i].answers[j] === userAnswers[i]) {
+                    answerList.style.color = "red";
+
+                }
+            }
+            if (userAnswers[i] === currentAnswers[i]) {
+                if (quiz[i].answers[j] === currentAnswers[i]) {
+                    answerList.style.color = "green";
+                }
+            }
+
+            answerCont.appendChild(answerList);
+        }
+        allQuestions.appendChild(questionCount)
+        allQuestions.appendChild(answerCont);
+
+    }
+
+}
+
+const closeModal = () => {
+    startBtn.classList.remove("hide")
+    mainView.classList.add("hide")
+    resltBtn.classList.add("hide")
+    modal.classList.add("hide")
+    location.reload();
+}
 
 
 
 
 startBtn.addEventListener("click", startQiuz)
 nextBtn.addEventListener("click", nextQuestion)
+resltBtn.addEventListener("click", quizResult)
+closeModals.addEventListener("click", closeModal)
+
+
